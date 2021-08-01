d0cc88e84f0caa667bc4ec17e2c53cefc5be19a1
Nabeel Raja <nabeel.raja@outlook.com>
2021-08-02 03:29:55 +0530

    updated script

--- scripts/generate-key.sh
+++ scripts/generate-key.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-mkdir "demo-device"
-cd demo-device
-openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem -nodes -out rsa_cert.pem -subj "/CN=unused"
+
+FILE=rsa_private.pem
+DIR=secrets
+
+mkdir -p $DIR
+cd $DIR
+
+if [ -f "$FILE" ]; then
+    echo "$FILE already exists. Do nothing!!"
+else 
+    echo "$FILE does not exist."
+    echo "Generating the key..."
+    openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem -nodes -out rsa_cert.pem -subj "/CN=unused"
+fi
