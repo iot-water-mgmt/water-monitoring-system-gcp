cbffd513d06dca81369952ab8320638aefa049c9
Nabeel Raja <nabeel.raja@outlook.com>
2021-08-02 23:57:35 +0530

    updated bash scripts

--- scripts/deregister-device.sh
+++ scripts/deregister-device.sh
@@ -5,7 +5,6 @@ REGION=$2
 REGISTRY=$3
 NUM_OF_DEVICES=$4
 
-cd demo-device
 counter=1
 while [ $counter -le $NUM_OF_DEVICES ]; do
     sudo gcloud iot devices delete device$counter --project=$PROJECT --region=$REGION --registry=$REGISTRY --quiet
